4119da419c34d039c402dcd70763b173aef26ce3
梶塚太智 <kajizukataichi@outlook.jp>
2024-10-31 22:27:56 +0900

    Create playmusic.js

---
+++ playmusic.js
@@ -0,0 +1,27 @@
+let tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    height: "360",
+    width: "640",
+    videoId: "5QNaSMiZ_-o",
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: function () {},
+    },
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.ENDED) {
+    console.log("動画の再生が終了しました");
+  }
+}
